13cc154f1d9116694774060bfcff1f040b33bfb8
Jeff Casimir <jeff@casimircreative.com>
2012-10-19 15:02:44 -0300

    Pending tests for ideas from README writing

---
+++ spec/locale/matcher_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe "LocaleSetter::Matcher" do
+  it "can properly match mixed case locales"
+  it "can fuzzy-match between locales with country and those without"
+end
