08662522c79009a45b1a7c4dccac43384c3f05bc
Jeff Casimir <jeff@casimircreative.com>
2012-10-19 09:57:43 -0300

    Setup first test harness

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in locale_setter.gemspec
 gemspec
+
+gem 'rspec'
---
+++ spec/locale_setter_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe LocaleSetter do
+  it "exists" do
+    expect{ LocaleSetter.new }.to_not raise_error
+  end
+end
