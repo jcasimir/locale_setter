6290fb67ba96385232a26824d021f30ba9621d05
Jeff Casimir <jeff@casimircreative.com>
2012-10-19 10:32:06 -0300

    Sketching the rails-centric bits

--- lib/locale_setter.rb
+++ lib/locale_setter.rb
@@ -1,5 +1,13 @@
 require "locale_setter/version"
 
 module LocaleSetter
-  # Your code goes here...
+  def self.included(controller)
+    if controller.respond_to?(:before_filter)
+      controller.before_filter :set_locale
+    end
+  end
+
+  def default_url_options(options)
+    {:locale => ""}.merge(options)
+  end
 end
--- spec/locale_setter_spec.rb
+++ spec/locale_setter_spec.rb
@@ -2,6 +2,43 @@ require 'spec_helper'
 
 describe LocaleSetter do
   it "exists" do
-    expect{ LocaleSetter.new }.to_not raise_error
+    expect{ LocaleSetter }.to_not raise_error
+  end
+
+  class BareController; end
+
+  describe ".included" do
+
+    it "sets a before filter" do
+      BareController.should_receive(:before_filter).with(:set_locale)
+      BareController.send(:include, LocaleSetter)
+    end
+
+    it "skips setting the before_filter if not supported" do
+      expect{ BareController.send(:include, LocaleSetter) }.to_not raise_error
+    end
+  end
+
+  class Controller
+    include LocaleSetter
+  end
+
+  describe "#default_url_options" do
+    let(:controller){ Controller.new }
+
+    it "adds a :locale key" do
+      controller.default_url_options({})[:locale].should be
+    end
+
+    it "builds on passed in options" do
+      result = controller.default_url_options({:test => true})
+      result[:test].should be
+      result[:locale].should be
+    end
+
+    it "defers to a passed in locale" do
+      result = controller.default_url_options({:locale => 'abc'})
+      result[:locale].should == 'abc'
+    end
   end
 end
