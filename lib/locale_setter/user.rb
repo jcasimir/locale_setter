0b0660e2c4f8a802de6658105383093537fa8f96
Jeff Casimir <jeff@casimircreative.com>
2012-11-29 09:25:40 -0500

    Making the user method configurable

--- lib/locale_setter/user.rb
+++ lib/locale_setter/user.rb
@@ -1,9 +1,20 @@
 module LocaleSetter
-  module User
+  class User
+    @@user_locale_method = :locale
+
     def self.for(user, available)
-      if user && user.respond_to?(:locale) && user.locale && !user.locale.empty?
-        LocaleSetter::Matcher.match(user.locale, available)
+      if user && user.respond_to?(locale_method) && 
+         user.send(locale_method) && !user.send(locale_method).empty?
+        LocaleSetter::Matcher.match user.send(locale_method), available
       end
+    end
+
+    def self.locale_method
+      @@user_locale_method
+    end
+
+    def self.locale_method=(method_name)
+      @@user_locale_method = method_name
     end
   end
 end
--- spec/locale/user_spec.rb
+++ spec/locale/user_spec.rb
@@ -16,5 +16,18 @@ describe 'LocaleSetter::User' do
       class NoLocaleUser; end
       LocaleSetter::User.for(NoLocaleUser.new, [:default]).should_not be
     end
+
+    it "uses a configurable field name" do
+      class MyLocaleUser
+        def my_locale
+          "arr"
+        end
+      end
+
+      LocaleSetter::User.locale_method = :my_locale
+
+      user = MyLocaleUser.new
+      LocaleSetter::User.for(user, ["arr"]).should == :arr
+    end
   end
 end
