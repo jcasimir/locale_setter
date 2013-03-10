9d2a1a36cc8400dd42f261e144968d4e9b6a2ae2
Steve Klabnik <steve@steveklabnik.com>
2013-03-10 09:51:16 -0700

    Fix for Ruby 1.8.7.
    We don't have Symbol#empty? there, so we'll just make one.
    
    This is the same implementation that Ruby 2.0 has:
    http://www.ruby-doc.org/core-2.0/Symbol.html\#method-i-empty-3F

--- lib/locale_setter/user.rb
+++ lib/locale_setter/user.rb
@@ -1,3 +1,13 @@
+# this is primarily for 1.8.7 support
+# see http://www.ruby-doc.org/core-2.0/Symbol.html#method-i-empty-3F
+unless :locale.respond_to? :empty?
+  class Symbol
+    def empty?
+      self.to_s.empty?
+    end
+  end
+end
+
 module LocaleSetter
   module User
     @@user_locale_method = :locale
