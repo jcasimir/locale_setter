ca41b3827125d87eecd4740707bc39b35791b497
diggabyte <scott@di.fm>
2013-10-28 17:02:36 -0600

    Update tailtie.rb to call ActionController::Base

--- lib/locale_setter/railtie.rb
+++ lib/locale_setter/railtie.rb
@@ -6,7 +6,7 @@ module LocaleSetter
       # I would prefer to do this in an initializer block, but it's important
       # that we do this _before_ any of the user's authentication stuff
       # happens. So this is the best we can get for now.
-      ApplicationController.send(:include, LocaleSetter::Controller)
+      ActionController::Base.send(:include, LocaleSetter::Controller)
     end
   end
   module Rails
